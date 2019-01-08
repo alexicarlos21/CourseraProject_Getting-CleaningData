d0cf233ac8a8465a82c626974a0f460e221cb96d
alexicarlos21 <44836716+alexicarlos21@users.noreply.github.com>
2019-01-08 23:24:03 +0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,15 @@
 ###################################################################################################
-## GETTING AND CLEANING DATA - COURSE PROJECT IN COURSERA							  #	  
-## Author: Alexi Carlos		 											  #
-## 																  #
-## This script does the following: 											  #
-##   0) Downloads, unzips, and reads data files from a given link					 	  #
-##   1) Merges the training and the test sets to create one data set.					  #
-##   2) Extracts only the measurements on the mean and standard deviation for each measurement.   #
-##   3) Uses descriptive activity names to name the activities in the data set			  #
-##   4) Appropriately labels the data set with descriptive variable names.				  #
-##   5) From the data set in step 4, creates a second, independent tidy data set with the 	  #
-##	  average of each variable for each activity and each subject.					  #					  
+## GETTING AND CLEANING DATA - COURSE PROJECT IN COURSERA							  	  
+## Author: Alexi Carlos		 											  
+## 																  
+## This script does the following: 											  
+##   0) Downloads, unzips, and reads data files from a given link					 	  
+##   1) Merges the training and the test sets to create one data set.					  
+##   2) Extracts only the measurements on the mean and standard deviation for each measurement.   
+##   3) Uses descriptive activity names to name the activities in the data set			  
+##   4) Appropriately labels the data set with descriptive variable names.				  
+##   5) From the data set in step 4, creates a second, independent tidy data set with the 	  
+##	  average of each variable for each activity and each subject.					  					  
 ###################################################################################################
 
 
